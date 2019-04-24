436248adc797e84d04d4fb8d3625f2490f5b62f2
JackMarx <seriousfools@gmail.com>
2019-04-24 11:40:21 -0500

    give all raises

--- manager.rb
+++ manager.rb
@@ -1,17 +1,3 @@
-
-
-# employee_1 = ["Helen", "Bonom Carter", 80000, true]
-# employee_2 = ["Peter", "Gibbons", 60000, true]
-
-# puts "#{ employee_1[0] } #{ employee_1[1] } makes #{ employee_1[2] } a year."
-# puts "#{ employee_2[0] } #{ employee_2[1] } makes #{ employee_2[2] } a year."
-
-# employee_1 = {:first_name => "Helen", :last_name => "Bonom Carter", :salary => 80000, :active => true}
-# employee_2 = {first_name: "Peter", last_name: "Gibbons", salary: 60000, active: true}
-
-# puts "#{ employee_1[:first_name] } #{ employee_1[:last_name] } makes #{ employee_1[:salary] } a year."
-# puts "#{ employee_2[:first_name] } #{ employee_2[:last_name] } makes #{ employee_2[:salary] } a year."
-
 class Employee
   attr_reader :first_name, :last_name, :salary, :active
   attr_writer :active
@@ -43,6 +29,12 @@ class Manager < Employee
     # code to send email
     puts "Email sent"
   end
+
+  def give_all_raises
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+  end
 end
 
 employee_1 = Employee.new(
@@ -67,8 +59,10 @@ manager = Manager.new(
                       employees: [employee_1, employee_2]
                       )
 
-p employee_1
-p employee_2
-p manager
+employee_1.print_info
+employee_2.print_info
 
+manager.give_all_raises
 
+employee_1.print_info
+employee_2.print_info
