ae41ceb1883d0fce5e86da601c8382d79f6dddef
JackMarx <seriousfools@gmail.com>
2019-04-24 09:32:42 -0500

    options hash

--- employee.rb
+++ employee.rb
@@ -16,11 +16,11 @@ class Employee
   attr_reader :first_name, :last_name, :salary, :active
   attr_writer :active
 
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
   def print_info
@@ -32,9 +32,22 @@ class Employee
   end
 end
 
-employee_1 = Employee.new("Helen", "Bonom Carter", 80000, true)
-employee_2 = Employee.new("Peter", "Gibbons", 60000, true)
+employee_1 = Employee.new(
+                          first_name: "Helen", 
+                          last_name: "Bonom Carter", 
+                          salary: 80000, 
+                          active: true
+                          )
 
+employee_2 = Employee.new(
+                          first_name: "Peter", 
+                          last_name: "Gibbons", 
+                          salary: 60000, 
+                          active: true
+                          )
+
+employee_1.print_info
+employee_2.print_info
 p employee_2.active
 employee_2.active = false
 p employee_2.active
