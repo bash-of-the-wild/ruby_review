4e8967f39f20a6c106ac1c8e270f833d9ecc264f
JackMarx <seriousfools@gmail.com>
2019-04-25 10:17:49 -0500

    seperate files

--- employee.rb
+++ employee.rb
@@ -1,17 +1,3 @@
-
-
-# employee_1 = ["Helen", "Bonom Carter", 80000, true]
-# employee_2 = ["Peter", "Gibbons", 60000, true]
-
-# puts "#{ employee_1[0] } #{ employee_1[1] } makes #{ employee_1[2] } a year."
-# puts "#{ employee_2[0] } #{ employee_2[1] } makes #{ employee_2[2] } a year."
-
-# employee_1 = {:first_name => "Helen", :last_name => "Bonom Carter", :salary => 80000, :active => true}
-# employee_2 = {first_name: "Peter", last_name: "Gibbons", salary: 60000, active: true}
-
-# puts "#{ employee_1[:first_name] } #{ employee_1[:last_name] } makes #{ employee_1[:salary] } a year."
-# puts "#{ employee_2[:first_name] } #{ employee_2[:last_name] } makes #{ employee_2[:salary] } a year."
-
 class Employee
   attr_reader :first_name, :last_name, :salary, :active
   attr_writer :active
@@ -31,44 +17,3 @@ class Employee
     @salary *= 1.05
   end
 end
-
-class Manager < Employee
-  def initialize(input_options)
-    super(input_options)
-    @employees = input_options[:employees]
-  end
-
-  def send_report
-    puts "Sending email..."
-    # code to send email
-    puts "Email sent"
-  end
-end
-
-employee_1 = Employee.new(
-                          first_name: "Helen", 
-                          last_name: "Bonom Carter", 
-                          salary: 80000, 
-                          active: true
-                          )
-
-employee_2 = Employee.new(
-                          first_name: "Peter", 
-                          last_name: "Gibbons", 
-                          salary: 60000, 
-                          active: true
-                          )
-
-manager = Manager.new(
-                      first_name: "Leslie",
-                      last_name: "Knope",
-                      salary: 100000,
-                      active: true,
-                      employees: [employee_1, employee_2]
-                      )
-
-p employee_1
-p employee_2
-p manager
-
-
